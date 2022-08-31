73ea1c71794ed159289986c7fd360f485214c7d4
anjalidalal <anjalimaria2002@gmail.com>
2022-08-31 21:54:27 +0530

    Mobile-app

--- src/components/PageOne.jsx
+++ src/components/PageOne.jsx
@@ -5,6 +5,7 @@ import chrome from "./images/chrome.png";
 import mail from "./images/mail.png";
 import google from "./images/google.png";
 import mic from "./images/mic.png";
+import weather from "./images/weather.png";
 import Footer from "./Footer";
 import Header from "./Header";
 
@@ -15,6 +16,17 @@ const PageOne = () => {
       <div className="google">
         <img src={google} width="29px" height="29px" />
         <img src={mic} width="30px" height="30px" />
+      </div>
+      <div className="time-weather">
+        <div className="time">
+          <h3>9:37</h3>
+          <p>Fri, Sep 2</p>
+        </div>
+        <div className="weather">
+          <img src={weather} />
+          <p>Cloudy 32°C</p>
+          <p>AQI 88</p>
+        </div>
       </div>
       <div className="frontpage-apps">
         <div className="icons">
