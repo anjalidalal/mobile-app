769aa86f3f049c070155284887a68a4f92a21002
anjalidalal <anjalimaria2002@gmail.com>
2022-08-31 21:02:43 +0530

    Mobile-app

--- src/components/PageOne.jsx
+++ src/components/PageOne.jsx
@@ -3,6 +3,8 @@ import youtube from "./images/youtube.png";
 import github from "./images/github.png";
 import chrome from "./images/chrome.png";
 import mail from "./images/mail.png";
+import google from "./images/google.png";
+import mic from "./images/mic.png";
 import Footer from "./Footer";
 import Header from "./Header";
 
@@ -10,6 +12,10 @@ const PageOne = () => {
   return (
     <>
       <Header />
+      <div className="google">
+        <img src={google} width="25px" height="25px" />
+        <img src={mic} width="28px" height="28px" />
+      </div>
       <div className="frontpage-apps">
         <div className="icons">
           <img src={mail} />
